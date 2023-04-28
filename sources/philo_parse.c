d2edbad7bb73a46311f20fbc1d494922260abe49
Beely0353 <bilal_aroun@outlook.fr>
2023-04-28 14:24:44 +0200

    310 200 100

--- sources/philo_parse.c
+++ sources/philo_parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo_parse.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: baroun <baroun@student.42.fr>              +#+  +:+       +#+        */
+/*   By: biaroun <biaroun@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 14:42:34 by baroun            #+#    #+#             */
-/*   Updated: 2022/11/07 19:04:29 by baroun           ###   ########.fr       */
+/*   Updated: 2023/04/24 17:57:49 by biaroun          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ t_table	*philo_parse(int ac, char **av)
 	table->nb_of_philo = ft_atoi(av[1]);
 	table->philo = malloc(sizeof(t_philo) * table->nb_of_philo);
 	table->t_die = ft_atoi(av[2]);
-	table->t_eat = ft_atoi(av[3]);
-	table->t_sleep = ft_atoi(av[4]);
+	table->t_eat = 10 + ft_atoi(av[3]);
+	table->t_sleep = 10 + ft_atoi(av[4]);
 	table->nb_of_full = 0;
 	table->end = 0;
 	pthread_mutex_init(&table->printing, NULL);
